76f6032777925a4298b03bc3cf237e51b1537735
MATHEUS SARMENTO PINTO <unifmpinto@feinet.fei.edu.br>
2025-10-21 21:55:15 -0300

    Roteamento para todas as telas

---
+++ src/app/component/info-livro/info-livro.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info-livro',
+  imports: [],
+  templateUrl: './info-livro.html',
+  styleUrl: './info-livro.css'
+})
+export class InfoLivro {
+
+}
---
+++ src/app/pages/emprestimos/emprestimos.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-emprestimos',
+  imports: [],
+  templateUrl: './emprestimos.html',
+  styleUrl: './emprestimos.css'
+})
+export class Emprestimos {
+
+}
