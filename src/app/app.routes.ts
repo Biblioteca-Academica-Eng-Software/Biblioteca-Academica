a2fc096fea49b8e94f1285420134633a271e7888
MATHEUS SARMENTO PINTO <unifmpinto@feinet.fei.edu.br>
2025-10-21 21:55:15 -0300

    Roteamento para todas as telas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Cadastro } from './pages/cadastro/cadastro';
+import { Home } from './pages/home/home';
+import { Emprestimos } from './pages/emprestimos/emprestimos';
+import { Contato } from './pages/contato/contato';
+import { ListaLivros } from './pages/lista-livros/lista-livros';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  { path: 'cadastro', component: Cadastro },
+  { path: 'contato', component: Contato },
+  { path: 'emprestimos', component: Emprestimos },
+  { path: 'home', component: Home },
+  { path: 'lista-livros', component: ListaLivros },
+//   { path: 'home', component: Home, canActivate: [AuthGuard] },
+//   { path: 'emprestimos', component: Emprestimos, canActivate: [AuthGuard] },
+  { path: '**', component: Login },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
