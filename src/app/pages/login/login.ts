471d0e6986fcdd179da460e6b4bd9cf4b5886cff
Matheus Sarmento <matheussarmento02@gmail.com>
2025-10-28 21:59:59 -0300

    pagina login e cadastro

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButton, MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [MatButtonModule, FormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.css'
+  styleUrl: './login.css',
 })
 export class Login {
+  public usuario = '';
+  public senha = '';
 
+  constructor(private router: Router) {}
+
+  public login() {}
+
+  public cadastro() {
+    this.router.navigate(['/cadastro']);
+  }
 }
